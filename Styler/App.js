396a38d269aa498876cfd1c121a0ff8543429517
iharshit009 <harshitjain1309@gmail.com>
2019-12-14 15:10:09 +0530

    coompleted the Styles app

--- Styler/App.js
+++ Styler/App.js
@@ -3,10 +3,18 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ flex: 1, flexDirection: "row" }}>
-      <View style={{ flex: 1, backgroundColor: "#2475B0" }} />
-      <View style={{ flex: 1, backgroundColor: "#10A881" }} />
-      <View style={{ flex: 2, backgroundColor: "#E74292" }} />
+    <View
+      style={{
+        flex: 1,
+        alignItems:"stretch",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginTop: 50
+      }}
+    >
+      <View style={{ width: 50, height: 50, backgroundColor: "#2475B0" }} />
+      <View style={{ width: 100, height: 100, backgroundColor: "#10A881" }} />
+      <View style={{ width: 150, height: 150, backgroundColor: "#E74292" }} />
     </View>
   );
 }
